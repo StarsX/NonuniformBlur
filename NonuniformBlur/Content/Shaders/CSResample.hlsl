d49e4d3cd799b69213c1bc7ed1fca88300402be8
Tianchen Xu <TianchenX@outlook.com>
2019-06-28 23:49:09 +0800

    High quality down-sampling filter (seems no extra cost, latency hidden).

--- NonuniformBlur/Content/Shaders/CSResample.hlsl
+++ NonuniformBlur/Content/Shaders/CSResample.hlsl
@@ -23,5 +23,22 @@ void main(uint2 DTid : SV_DispatchThreadID )
 	g_txDest.GetDimensions(dim.x, dim.y);
 
 	const float2 tex = (DTid + 0.5) / dim;
-	g_txDest[DTid] = g_txSource.SampleLevel(g_smpLinear, tex, 0);
+
+#ifdef _HIGH_QUALITY_
+	float4 srcs[5];
+	srcs[0] = g_txSource.SampleLevel(g_smpLinear, tex, 0.0);
+	srcs[1] = g_txSource.SampleLevel(g_smpLinear, tex, 0.0, int2(-1, 0));
+	srcs[2] = g_txSource.SampleLevel(g_smpLinear, tex, 0.0, int2(1, 0));
+	srcs[3] = g_txSource.SampleLevel(g_smpLinear, tex, 0.0, int2(0, -1));
+	srcs[4] = g_txSource.SampleLevel(g_smpLinear, tex, 0.0, int2(0, 1));
+
+	float4 result = srcs[0] * 2.0;
+	[unroll]
+	for (uint i = 1; i < 5; ++i) result += srcs[i];
+	result /= 6.0;
+#else
+	const float4 result = g_txSource.SampleLevel(g_smpLinear, tex, 0.0);
+#endif
+
+	g_txDest[DTid] = result;
 }
