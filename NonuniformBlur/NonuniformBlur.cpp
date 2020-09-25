2fc6f90a32e068e059babf92bea7f5432e14cc99
StarsX <X-Universe@outlook.com>
2020-09-25 09:32:56 +0800

    Update XUSG with stencil SRV fix.

--- NonuniformBlur/NonuniformBlur.cpp
+++ NonuniformBlur/NonuniformBlur.cpp
@@ -145,7 +145,7 @@ void NonUniformBlur::LoadPipeline(vector<Resource>& uploaders)
 	swapChainDesc.BufferCount = FrameCount;
 	swapChainDesc.Width = m_width;
 	swapChainDesc.Height = m_height;
-	swapChainDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+	swapChainDesc.Format = GetDXGIFormat(Format::B8G8R8A8_UNORM);
 	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
 	swapChainDesc.SampleDesc.Count = 1;
