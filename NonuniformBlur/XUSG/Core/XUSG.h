6e2c2e6bd2aabea573d69515f643c07a594b3f50
StarsX <X-Universe@outlook.com>
2021-02-27 16:11:45 +0800

    Update XUSG header to change default descriptor flag.

--- NonuniformBlur/XUSG/Core/XUSG.h
+++ NonuniformBlur/XUSG/Core/XUSG.h
@@ -1389,11 +1389,11 @@ namespace XUSG
 			virtual void SetConstants(uint32_t index, uint32_t num32BitValues, uint32_t binding,
 				uint32_t space = 0, Shader::Stage stage = Shader::Stage::ALL) = 0;
 			virtual void SetRootSRV(uint32_t index, uint32_t binding, uint32_t space = 0,
-				DescriptorFlag flags = DescriptorFlag::DATA_STATIC, Shader::Stage stage = Shader::Stage::ALL) = 0;
+				DescriptorFlag flags = DescriptorFlag::DATA_STATIC_WHILE_SET_AT_EXECUTE, Shader::Stage stage = Shader::Stage::ALL) = 0;
 			virtual void SetRootUAV(uint32_t index, uint32_t binding, uint32_t space = 0,
-				DescriptorFlag flags = DescriptorFlag::NONE, Shader::Stage stage = Shader::Stage::ALL) = 0;
+				DescriptorFlag flags = DescriptorFlag::DATA_STATIC_WHILE_SET_AT_EXECUTE, Shader::Stage stage = Shader::Stage::ALL) = 0;
 			virtual void SetRootCBV(uint32_t index, uint32_t binding, uint32_t space = 0,
-				DescriptorFlag flags = DescriptorFlag::NONE, Shader::Stage stage = Shader::Stage::ALL) = 0;
+				DescriptorFlag flags = DescriptorFlag::DATA_STATIC, Shader::Stage stage = Shader::Stage::ALL) = 0;
 
 			virtual XUSG::PipelineLayout CreatePipelineLayout(PipelineLayoutCache& pipelineLayoutCache, PipelineLayoutFlag flags,
 				const wchar_t* name = nullptr) = 0;
